c52cb0c8bc4be8f3e7c4e585bc39fccb45a38dca
Maarten van Gompel <proycon@anaproy.nl>
2013-02-12 20:32:37 +0100

    added

---
+++ initvariants.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+data = {}
+
+f = codecs.open('data/targetwords','r','utf-8')
+for line in f:
+    fields = line.split('\t')
+    lang = fields[2]
+    if not lang in data:
+        data[lang] = set()
+    data[lang] = data[lang] | set(fields[3].split(';'))    
+f.close()
+
+for lang, senses in data.items():
+    if not os.path.exists('data/' + lang + '.variants'):
+        f = codecs.open('data/' + lang + '.variants','w')
+        for sense in sense:
+            f.write(sense + '\n')
+        f.close()    
+        
