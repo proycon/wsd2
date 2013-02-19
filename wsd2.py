75e2d0c387e15b0717d9ccfddbd0eeb282692ebd
Maarten van Gompel <proycon@anaproy.nl>
2013-02-19 16:13:27 +0100

    test

--- wsd2.py
+++ wsd2.py
@@ -514,7 +514,7 @@ class CLWSD2Tester(object):
                             mindistance = distance 
                            
                 if focusindex == -1: 
-                    raise Exception("Focus word not found after tokenisation! This should not happen! head=" + head.encode('utf-8'))
+                    raise Exception("Focus word not found after tokenisation! This should not happen! head=" + head.encode('utf-8') + ",words=" + " ".join(sourcewords).encode('utf-8'))
                          
                 #grab local context features
                 features = []                    
