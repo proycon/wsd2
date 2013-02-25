c44755eb79eedaf6999e02f0f8a33a25bade64ba
Maarten van Gompel <proycon@anaproy.nl>
2013-02-25 15:50:20 +0100

    error output

--- wsd2.py
+++ wsd2.py
@@ -782,12 +782,14 @@ class CLWSD2Tester(object):
         print >>sys.stderr, "Scoring"
         for lemma,pos in self.testset.lemmas():  
             print >>sys.stderr, "Scoring " + lemma.encode('utf-8')
-            r = os.system('perl ' + WSDDIR + '/ScorerTask3.pl ' + self.outputdir + '/' + lemma + '.' + pos + '.best' + ' ' + WSDDIR + '/data/trial/' + self.targetlang + '/' + lemma + '_gold.txt 2> ' + outputdir + '/' + lemma + '.' + pos + '.best.scorerr')
+            cmd = 'perl ' + WSDDIR + '/ScorerTask3.pl ' + self.outputdir + '/' + lemma + '.' + pos + '.best' + ' ' + WSDDIR + '/data/trial/' + self.targetlang + '/' + lemma + '_gold.txt 2> ' + outputdir + '/' + lemma + '.' + pos + '.best.scorerr'
+            r = os.system(cmd)
             if r != 0:
-                print >>sys.stderr,"ERROR: SCORER FAILED! INSPECT " + outputdir + '/' + lemma + '.' + pos + '.best.scorerr' 
-            r = os.system('perl ' + WSDDIR + '/ScorerTask3.pl ' + self.outputdir + '/' + lemma + '.' + pos + '.oof' + ' ' + WSDDIR + '/data/trial/' + self.targetlang + '/' + lemma + '_gold.txt -t oof 2> ' + outputdir + '/' + lemma + '.' + pos + '.oof.scorerr')
+                print >>sys.stderr,"ERROR: SCORER FAILED! INSPECT " + outputdir + '/' + lemma + '.' + pos + '.best.scorerr -- Command was: ' + cmd 
+            cmd = 'perl ' + WSDDIR + '/ScorerTask3.pl ' + self.outputdir + '/' + lemma + '.' + pos + '.oof' + ' ' + WSDDIR + '/data/trial/' + self.targetlang + '/' + lemma + '_gold.txt -t oof 2> ' + outputdir + '/' + lemma + '.' + pos + '.oof.scorerr'
+            r = os.system(cmd)
             if r != 0:
-                print >>sys.stderr,"ERROR: SCORER FAILED! INSPECT " + outputdir + '/' + lemma + '.' + pos + '.oof.scorerr'            
+                print >>sys.stderr,"ERROR: SCORER FAILED! INSPECT " + outputdir + '/' + lemma + '.' + pos + '.oof.scorerr -- Command was: ' + cmd             
             
         scorereport(self.outputdir)            
                  
