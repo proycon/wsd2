adc9a6953dfcd2141e9de6f247e7a27b53aa1540
Maarten van Gompel <proycon@anaproy.nl>
2013-03-01 10:49:35 +0100

    update

--- collectresults.py
+++ collectresults.py
@@ -9,6 +9,7 @@ import codecs
 basedir = sys.argv[1]
 
 languages = ['nl','es','it','de','fr']
+confs = set()
 
 bestdata = {}
 oofdata = {}
@@ -20,6 +21,7 @@ for lang in languages:
     for confdir in glob.glob(basedir + '/' + lang + '/c*'):
         if os.path.isdir(confdir):
             conf = os.path.basename(confdir)
+            confs.add(conf)
             if os.path.exists(confdir + '/results'):
                 mode = ''
                 f = codecs.open(confdir + '/results','r','utf-8')
@@ -45,4 +47,47 @@ for lang in languages:
                                     oofdata[lang][lemma] = {}
                                 oofdata[lang][lemma][conf] = score                                
                     f.close()
+
+
+print "TYPE\tLANG\tLEMMA\tFIRST\tSECOND\tTHIRD",
+for conf in sorted(confs):
+    print "\t" + conf,
+print
+
+for lang in languages:
+    if lang in bestdata:
+        lemmas = set(bestdata[lang]) | set(oofdata[lang])
+        
+        for lemma in sorted(lemmas):
+            print "best\t" + lang + "\t" + lemma.encode('utf-8')
+            if lemma in bestdata[lang]:
+                for x in sorted(bestdata[lang][lemma].items(), key=x[1] * -1)[:3]: 
+                    print "\t" + x,
+                for conf in sorted(confs):
+                    if conf in bestdata[lang][lemma]:
+                        print "\t" + str(bestdata[lang][lemma][conf]),
+                    else:
+                        print "\t0",
+            else:
+                print "-\t-\t-",
+                for conf in sorted(confs):
+                    print "\t0",
+            print
+            
+            print "oof\t" + lang + "\t" + lemma.encode('utf-8')
+            if lemma in oofdata[lang]:
+                for x in sorted(oofdata[lang][lemma].items(), key=x[1] * -1)[:3]: 
+                    print "\t" + x,
+                for conf in sorted(confs):
+                    if conf in oofdata[lang][lemma]:
+                        print "\t" + str(oofdata[lang][lemma][conf]),
+                    else:
+                        print "\t0",
+            else:
+                print "-\t-\t-",
+                for conf in sorted(confs):
+                    print "\t0",
+            print            
+            
+            
 
